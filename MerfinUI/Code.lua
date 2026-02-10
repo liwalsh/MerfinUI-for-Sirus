cb573d81d6b6e596f08715ffcac58e854c9c7710
Vladimir <vl4d1m1r0ff@gmail.com>
2026-02-10 20:36:00 +0700

    Add files via upload

--- MerfinUI/Code.lua
+++ MerfinUI/Code.lua
@@ -1,9 +1,7 @@
 local E, L, V, P, G = unpack(ElvUI)
 local EP = LibStub("LibElvUIPlugin-1.0")
 local PI = E:GetModule('PluginInstaller')
-
 local addonName, addonTable = ...
-
 addonTable.L = GetLocale()
 addonTable.Name = '|cff40c7ebMerfinUI|r'
 addonTable.Version = tonumber(GetAddOnMetadata(addonName, 'Version'))
@@ -12,18 +10,13 @@ addonTable.Texture = 'Flatt'
 addonTable.AceProfileName = string.format("%s - %s", UnitName("player"), GetRealmName("player"))
 addonTable.MerfinProfileName = 'MerfinUI v' .. addonTable.Version
 addonTable.RealmName = GetRealmName("player")
-
 MUI = E:NewModule(addonName, 'AceConsole-3.0', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0')
-
 local Initialize = function()
-
 	if E.private.MUI.install_version == nil then
 		PI:Queue(MUI.InstallerData)
 	end
-
 	EP:RegisterPlugin(addonName, MUI.Config)
 	MUI:RegisterCommands()
 	MUI:RegisterEvents()
 end
-
 E:RegisterModule(addonName, Initialize)
