bdfadadd7044ac3d5f774aa52ddb5cdb2821baed
Vladimir <vl4d1m1r0ff@gmail.com>
2026-04-25 15:15:14 +0700

    Add files via upload

--- MerfinUI/Modules/CVar.lua
+++ MerfinUI/Modules/CVar.lua
@@ -2,37 +2,37 @@ local addonName, addonTable = ...
 local E, L, V, P, G = unpack(ElvUI)
 local SetCVar = SetCVar
 function MUI:Set_CVars()
-    local CVars = {
-        ["autoLootDefault"] = 1,
-        ["cameraPivot"] = 1,
-        ["cameraSmoothStyle"] = 0,
-        ["cameraDistanceMax"] = 50,
-        ["cameraPitchMoveSpeed"] = 70,
-        ["cameraYawMoveSpeed"] = 160,
-        ["CombatLogPeriodicSpells"] = 0,
-        ["chatStyle"] = "classic",
-        ["scriptErrors"] = 1,
-        ["showTutorials"] = 0,
-        ["showItemLevel"] = 1,
-        ["ShowAllSpellRanks"] = 0,
-        ["showNewbieTips"] = 0,
-        ["UnitNameNonCombatCreatureName"] = 0,
-        ["UnitNameFriendlyTotemName"] = 1,
-        ["UnitNameFriendlyGuardianName"] = 1,
-        ["UnitNameEnemyTotemName"] = 1,
-        ["UnitNameEnemyGuardianName"] = 1,
-        ['nameplateShowEnemies'] = 1,
-        ['nameplateShowFriends'] = 1,
-        ['nameplateShowFriendlyPets'] = 0,
-        ['nameplateShowFriendlyTotems'] = 1,
-        ['nameplateShowFriendlyGuardians'] = 0,
-        ["UnitNameNPC"] = 1,
-        ["UnitNameOwn"] = 0,
-        ["alwaysShowActionBars"] = 1,
-        ["ShowVKeyCastbar"] = 1
-    }
-    for CVar, variable in pairs(CVars) do
-        SetCVar(CVar, variable)
-    end
-    addonTable:PluginInstallStepComplete("Account Settings")
+	local CVars = {
+		["autoLootDefault"] = 1,
+		["cameraPivot"] = 1,
+		["cameraSmoothStyle"] = 0,
+		["cameraDistanceMax"] = 50,
+		["cameraPitchMoveSpeed"] = 70,
+		["cameraYawMoveSpeed"] = 160,
+		["CombatLogPeriodicSpells"] = 0,
+		["chatStyle"] = "classic",
+		["scriptErrors"] = 1,
+		["showTutorials"] = 0,
+		["showItemLevel"] = 1,
+		["ShowAllSpellRanks"] = 0,
+		["showNewbieTips"] = 0,
+		["UnitNameNonCombatCreatureName"] = 0,
+		["UnitNameFriendlyTotemName"] = 1,
+		["UnitNameFriendlyGuardianName"] = 1,
+		["UnitNameEnemyTotemName"] = 1,
+		["UnitNameEnemyGuardianName"] = 1,
+		['nameplateShowEnemies'] = 1,
+		['nameplateShowFriends'] = 1,
+		['nameplateShowFriendlyPets'] = 0,
+		['nameplateShowFriendlyTotems'] = 1,
+		['nameplateShowFriendlyGuardians'] = 0,
+		["UnitNameNPC"] = 1,
+		["UnitNameOwn"] = 0,
+		["alwaysShowActionBars"] = 1,
+		["ShowVKeyCastbar"] = 1
+	}
+	for CVar, variable in pairs(CVars) do
+		SetCVar(CVar, variable)
+	end
+	addonTable:PluginInstallStepComplete("Account Settings")
 end
--- MerfinUI/Modules/Chat.lua
+++ MerfinUI/Modules/Chat.lua
@@ -16,17 +16,17 @@ local JoinChannelByName = JoinChannelByName
 local ChatFrame_RemoveChannel = ChatFrame_RemoveChannel
 local ChatFrame_AddChannel = ChatFrame_AddChannel
 function MUI:SetupChat()
-    FCF_ResetChatWindows()
-    FCF_OpenNewWindow()
-    FCF_OpenNewWindow()
-    FCF_OpenNewWindow()
-    FCF_OpenNewWindow()
-    local fontSize = 15
+	FCF_ResetChatWindows()
+	FCF_OpenNewWindow()
+	FCF_OpenNewWindow()
+	FCF_OpenNewWindow()
+	FCF_OpenNewWindow()
+	local fontSize = 15
 	for _, name in ipairs(_G.CHAT_FRAMES) do
 		local frame = _G[name]
 		local id = frame:GetID()
-        if E.private.chat.enable then
-            CH:UpdateChatTabs()
+		if E.private.chat.enable then
+			CH:UpdateChatTabs()
 		end
 		FCF_SetChatWindowFontSize(nil, frame, fontSize)
 		if id == 1 then
@@ -39,10 +39,10 @@ function MUI:SetupChat()
 			FCF_SetWindowName(frame, 'GLD')
 		elseif id == 5 then
 			FCF_SetWindowName(frame, '/W')
-        elseif id == 6 then
-            FCF_SetWindowName(frame, 'GLB')
-        end
-    end
+		elseif id == 6 then
+			FCF_SetWindowName(frame, 'GLB')
+		end
+	end
 	chatGroup = {"SAY", "EMOTE", "YELL", "WHISPER", "PARTY", "PARTY_LEADER", "RAID", "RAID_LEADER", "RAID_WARNING", "BATTLEGROUND", "BATTLEGROUND_LEADER", "GUILD", "OFFICER", "ACHIEVEMENT", "GUILD_ACHIEVEMENT"}
 	for i = 1, MAX_WOW_CHAT_CHANNELS do
 		tinsert(chatGroup, "CHANNEL"..i)
@@ -50,30 +50,30 @@ function MUI:SetupChat()
 	for _, v in ipairs(chatGroup) do
 		ToggleChatColorNamesByClassGroup(true, v)
 	end
-    local chats = { 'LOOT', 'MONEY'}
-    ChatFrame_RemoveAllMessageGroups(_G.ChatFrame3)
-    for _, k in ipairs(chats) do
-        ChatFrame_AddMessageGroup(_G.ChatFrame3, k)
-    end
-    local chats = { 'GUILD', 'OFFICER', 'GUILD_ACHIEVEMENT'}
-    ChatFrame_RemoveAllMessageGroups(_G.ChatFrame4)
-    for _, k in ipairs(chats) do
-        ChatFrame_AddMessageGroup(_G.ChatFrame4, k)
-    end
-    local chats = { 'WHISPER', 'WHISPER_INFORM' }
-    ChatFrame_RemoveAllMessageGroups(_G.ChatFrame5)
-    for _, k in ipairs(chats) do
-        ChatFrame_AddMessageGroup(_G.ChatFrame5, k)
-    end
-    local chats = {  'GLOBAL', 'GENERAL' }
-    ChatFrame_RemoveAllMessageGroups(_G.ChatFrame6)
-        for _, k in ipairs(chats) do
-        ChatFrame_AddMessageGroup(_G.ChatFrame6, k)
-    end
-    FCFDock_SelectWindow(_G.GENERAL_CHAT_DOCK, _G.ChatFrame1)
-    ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'GENERAL')
-    ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'GLOBAL')
-    ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'LOOT')
-    ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'MONEY')
-    addonTable:PluginInstallStepComplete("Chat Settings")
+	local chats = {'LOOT', 'MONEY'}
+	ChatFrame_RemoveAllMessageGroups(_G.ChatFrame3)
+	for _, k in ipairs(chats) do
+		ChatFrame_AddMessageGroup(_G.ChatFrame3, k)
+	end
+	local chats = {'GUILD', 'OFFICER', 'GUILD_ACHIEVEMENT'}
+	ChatFrame_RemoveAllMessageGroups(_G.ChatFrame4)
+	for _, k in ipairs(chats) do
+		ChatFrame_AddMessageGroup(_G.ChatFrame4, k)
+	end
+	local chats = {'WHISPER', 'WHISPER_INFORM'}
+	ChatFrame_RemoveAllMessageGroups(_G.ChatFrame5)
+	for _, k in ipairs(chats) do
+		ChatFrame_AddMessageGroup(_G.ChatFrame5, k)
+	end
+	local chats = {'GLOBAL', 'GENERAL'}
+	ChatFrame_RemoveAllMessageGroups(_G.ChatFrame6)
+		for _, k in ipairs(chats) do
+		ChatFrame_AddMessageGroup(_G.ChatFrame6, k)
+	end
+	FCFDock_SelectWindow(_G.GENERAL_CHAT_DOCK, _G.ChatFrame1)
+	ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'GENERAL')
+	ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'GLOBAL')
+	ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'LOOT')
+	ChatFrame_RemoveMessageGroup(_G.ChatFrame1, 'MONEY')
+	addonTable:PluginInstallStepComplete("Chat Settings")
 end
