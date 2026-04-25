d5fc333818929e90af775ef51d1e6d0c5eeb7084
Vladimir <vl4d1m1r0ff@gmail.com>
2026-04-25 15:14:55 +0700

    Add files via upload

--- MerfinUI/Options/defaults.lua
+++ MerfinUI/Options/defaults.lua
@@ -2,8 +2,8 @@ local E, L, V, P, G = unpack(ElvUI)
 local addonName, addonTable = ...
 
 MUI.resolutions = {
-  FULL_HD = 'Full HD (1920-1080px)',
-  QUAD_HD = 'Quad HD (2560-1440px)',
+	FULL_HD = 'Full HD (1920-1080px)',
+	QUAD_HD = 'Quad HD (2560-1440px)',
 }
 
 G.MUI = {
@@ -11,31 +11,31 @@ G.MUI = {
 }
 
 V.MUI = {
-    general = {
-        profileSettings = {
+	general = {
+		profileSettings = {
 			install = {
 			},
-            media = {
+			media = {
 				resolution = addonTable.Resolution,
 				font = addonTable.Font,
 				texture = addonTable.Texture,
 			},
-            actionbars = {
-                showGrid = false,
-                showMouseover = true,
-            },
-        },
-        layout = {
-            dark = {
-                color = { r = 0.1803921568627451, g = 0.1607843137254902, b = 0.1607843137254902, a = 1 },
-                backdrop = { r = 0.5490196078431373, g = 0.4549019607843137, b = 0.4549019607843137, a = 1 },
-                dead = { r = 1, g = 0.25098039215686, b = 0.25098039215686, a = 1 },
-            }
-        },
-    },
-    automatation = {
-        deleteFill = true,
-    },
+			actionbars = {
+				showGrid = false,
+				showMouseover = true,
+			},
+		},
+		layout = {
+			dark = {
+				color = { r = 0.1803921568627451, g = 0.1607843137254902, b = 0.1607843137254902, a = 1 },
+				backdrop = { r = 0.5490196078431373, g = 0.4549019607843137, b = 0.4549019607843137, a = 1 },
+				dead = { r = 1, g = 0.25098039215686, b = 0.25098039215686, a = 1 },
+			}
+		},
+	},
+	automatation = {
+		deleteFill = true,
+	},
 	unitframes = {
 		transparentHealth = false,
 	},
