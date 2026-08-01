60c3b6b8d7e5eb1dea302da58bf4a1cf30fdf2b2
Vladimir <vl4d1m1r0ff@gmail.com>
2026-08-02 01:19:19 +0700

    Update functions.lua

--- MerfinUI/Options/functions.lua
+++ MerfinUI/Options/functions.lua
@@ -90,12 +90,7 @@ function MUI:ChangeTheme(theme)
 		E.db.unitframe.colors.health_backdrop_dead.g = dead.g
 		E.db.unitframe.colors.health_backdrop_dead.r = dead.r
 
-		if layout == 'DPS/Tank' or layout == 'Healer-V' then
-			E.db.unitframe.units['party'].customTexts.UnitName.text_format = "[namecolor][name]"
-		elseif layout == 'Healer-H' then
-			E.db.unitframe.units['party'].customTexts.UnitName.text_format = "[namecolor][name:abbrev:veryshort]"
-		end
-		
+		E.db.unitframe.units['party'].customTexts.UnitName.text_format = "[namecolor][name:abbrev:veryshort]"
 		E.db.unitframe.units['raid10'].customTexts.UnitName.text_format = "[namecolor][name:veryshort]"
 		E.db.unitframe.units['raid25'].customTexts.UnitName.text_format = "[namecolor][name:veryshort]"
 		E.db.unitframe.units['raid40'].customTexts.UnitName.text_format = "[namecolor][name:veryshort]"
@@ -123,12 +118,6 @@ function MUI:ChangeTheme(theme)
 		E.db.unitframe.colors.health_backdrop_dead.b = 0.46274509803922
 		E.db.unitframe.colors.health_backdrop_dead.g = 0.46274509803922
 		E.db.unitframe.colors.health_backdrop_dead.r = 0.51764705882353
-
-		if layout == 'DPS/Tank' or layout == 'Healer-V' then
-			E.db.unitframe.units['party'].customTexts.UnitName.text_format = "[name]"
-		elseif layout == 'Healer-H' then
-			E.db.unitframe.units['party'].customTexts.UnitName.text_format = "[name:abbrev:veryshort]"
-		end
 
 		E.db.unitframe.units['party'].customTexts.UnitName.text_format = "[name:abbrev:veryshort]"
 		E.db.unitframe.units['raid10'].customTexts.UnitName.text_format = "[name:veryshort]"
